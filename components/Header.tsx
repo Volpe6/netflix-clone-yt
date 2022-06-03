3b9ef59443455af00130bc65f129e8d6ec209bb8
Volpe6 <baasch2014@gmail.com>
2022-06-02 21:59:04 -0300

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,54 @@
+import { SearchIcon, BellIcon } from '@heroicons/react/solid';
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+
+function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  });
+  
+  
+  return (
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+        <div className="flex itens-center space-x-2 md:space-x-10">
+            <img  
+                src="https://rb.gy/ulxxee"
+                width={100} 
+                height={100} 
+            />
+
+            <ul className="hidden space-x-4 md:flex">
+                <li className="headerlink">Home</li>
+                <li className="headerlink">TV Shows</li>
+                <li className="headerlink">Movies</li>
+                <li className="headerlink">New & Pupolar</li>
+                <li className="headerlink">My List</li>
+            </ul>
+        </div>
+
+        <div className='flex items-center space-x-4 text-sm font-light'>
+          <SearchIcon className="hidden sm:inline w-6 h-6"/>
+          <p className="hidden lg:inline">Kids</p>
+          <BellIcon className='h-6 w-6'/>
+          <Link href="/account">
+            <img src="https://rb.gy/g1pwyx" alt="" className='cursor-pointer rounded'/>
+          </Link>
+        </div>
+    </header>
+  )
+}
+
+export default Header;
---
+++ components/Thumbnail.tsx
@@ -0,0 +1,32 @@
+import { Movie } from '../typings';
+import Image from "next/image";
+
+interface Props {
+    // movie: Movie | DocumentData
+    movie: Movie
+}
+
+function Thumbnail({ movie }: Props) {
+    // const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+    // const [showModal, setShowModal] = useRecoilState(modalState)
+  
+    return (
+      <div
+        className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
+        // onClick={() => {
+        //   setCurrentMovie(movie)
+        //   setShowModal(true)
+        // }}
+      >
+        <Image
+          src={`https://image.tmdb.org/t/p/w500${
+            movie.backdrop_path || movie.poster_path
+          }`}
+          className="rounded-sm object-cover md:rounded"
+          layout="fill"
+        />
+      </div>
+    )
+  }
+
+export default Thumbnail;
